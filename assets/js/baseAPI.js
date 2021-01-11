4f77187970076484815b5966e412799ab7c63193
sunyhzy <1279903393@.qq.com>
2021-01-11 11:46:49 +0800

    成功了

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -15,7 +15,7 @@ $.ajaxPrefilter(function(options) {
     // }
     // 全局统一挂载 complete 回调函数
     options.complete = function(res) {
-        // console.log('执行了 complete 回调：')
+        // console.log('执行了 ')
         // console.log(res)
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
